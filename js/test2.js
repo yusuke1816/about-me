9c96e00fa195111445b688341cee6efe67b5f32b
yusukezuck <yilcp76211@yahoo.co.jp>
2024-07-01 00:35:23 +0900

    indexadd

--- js/test2.js
+++ js/test2.js
@@ -1,14 +1,14 @@
 const swiper = new Swiper(".swiper", {
-    loop: true,
-    // ページネーション
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    // 前後の矢印
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    slidesPerView: 3,
-  });
+  loop: true,
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 3,
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
