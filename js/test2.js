ce49fba46df0ebaf100dea130cb52c55c0f25553
yusuke1816 <KTC24A31G0014@edu.kyoto-tech.ac.jp>
2024-07-05 16:30:51 +0900

    aa

--- js/test2.js
+++ js/test2.js
@@ -5,12 +5,35 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  slidesPerView: 3,
+  slidesPerView: 1,
   // 前後の矢印
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  centeredSlides: true,
+  spacebetween: 16,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1480: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    }
+  }
 });
 
 
