98dae9cd8fbd5680d550cb08850accc7cbc7109e
yusukezuck <yilcp76211@yahoo.co.jp>
2024-07-17 07:50:40 +0900

    あああ

--- js/test2.js
+++ js/test2.js
@@ -1,47 +1,53 @@
-
-
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".swiper1", {
   loop: true,
-  // ページネーション
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+      el: ".swiper1-pagination",
+      clickable: true,
   },
   slidesPerView: 1,
-  // 前後の矢印
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+      nextEl: ".swiper1-button-next",
+      prevEl: ".swiper1-button-prev",
   },
   centeredSlides: true,
-  spacebetween: 16,
+  spaceBetween: 16,
   breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 40
-    },
-    1480: {
-      slidesPerView: 3.1,
-      spaceBetween: 10,
-    }
-  }
+      320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+      },
+      480: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+      },
+      640: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+      },
+      1480: {
+          slidesPerView: 3.1,
+          spaceBetween: 10,
+      },
+  },
 });
 
 
 
 
 
+const swiper2 = new Swiper(".swiper2", {
+  pagination: {
+      el: ".swiper2-pagination",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper2-button-next",
+      prevEl: ".swiper2-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: false,
+});
 
 
 
