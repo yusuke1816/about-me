8c8a33056be3703dbd18513bf8d9d80296d2ae84
yusukezuck <yilcp76211@yahoo.co.jp>
2024-08-10 00:53:34 +0900

    footer

--- js/movie.js
+++ js/movie.js
@@ -17,13 +17,9 @@ const swiper1 = new Swiper(".swiper1", {
             spaceBetween: 20,
         },
         375: {
-          slidesPerView: 2,
-          spaceBetween: 30,
+            slidesPerView: 2,
+            spaceBetween: 30,
         },
-        400: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-      },
         480: {
             slidesPerView: 2,
             spaceBetween: 30,
@@ -32,14 +28,59 @@ const swiper1 = new Swiper(".swiper1", {
             slidesPerView: 3,
             spaceBetween: 40,
         },
-        1280:{
-          slidesPerView: 4,
+        1280: {
+            slidesPerView: 4,
         },
         1480: {
             slidesPerView: 5,
             spaceBetween: 10,
         },
     },
-  });
-  
+});
 
+$(document).ready(function() {
+    $('.open-modal').click(function(event) {
+        event.preventDefault();
+        var imgSrc = $(this).data('img-src');
+        var imgAlt = $(this).data('img-alt');
+
+        $('#modalImage').attr('src', imgSrc);
+        $('#modalCaption').text(imgAlt);
+        $('#customModal').fadeIn();
+    });
+
+    $('.close').click(function() {
+        $('#customModal').fadeOut();
+    });
+
+    $(window).click(function(event) {
+        if ($(event.target).is('#customModal')) {
+            $('#customModal').fadeOut();
+        }
+    });
+});
+
+
+
+    // Open modal when image link is clicked
+    $('.open-modal').click(function(event) {
+        event.preventDefault();
+        var imgSrc = $(this).data('img-src');
+        var imgAlt = $(this).data('img-alt');
+
+        $('#modalImage').attr('src', imgSrc);
+        $('#modalCaption').text(imgAlt);
+        $('#customModal').fadeIn();
+    });
+
+    // Close modal when the close button is clicked
+    $('.close').click(function() {
+        $('#customModal').fadeOut();
+    });
+
+    // Close modal when clicking outside the modal content
+    $(window).click(function(event) {
+        if ($(event.target).is('#customModal')) {
+            $('#customModal').fadeOut();
+        }
+    });
